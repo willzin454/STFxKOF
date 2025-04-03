52bdb14328a78009670195b6e75c07c619e1d97f
willzin454 <williamcarvalho493@gmail.com>
2025-04-02 21:57:58 -0300

    adição de cenario

--- src/index.js
+++ src/index.js
@@ -6,29 +6,37 @@ const GameViewport = {
 window.onload = function() {
     const canvasEl = document.querySelector('canvas');
     const context = canvasEl.getContext('2d'); 
-    const ryu = document.querySelector('img');
+    const [ryu, riustage] = document.querySelectorAll('img');
 
     canvasEl.width  = GameViewport.width;
     canvasEl.height = GameViewport.height;
 
     const position = {
-        x: 0,
-        y: 0,
+        x: GameViewport.width / 2 - ryu.width / 2,
+        y: 100,
     }
 
-    function frame(){
-        position.x += 1;
+    let velocity = 2;
 
+    function frame(){
+        position.x += velocity;
+
+        if(position.x > GameViewport.width - ryu.width || position.x < 0){
+            velocity = -velocity;
+        }
+
+        context.drawImage(riustage, 0, 0);  
+        // context.clearRect(0, 0, GameViewport.width, GameViewport.height);
         context.strokeStyle = 'yellow';
-        context.moveTo(0, 0);
-        context.lineTo(GameViewport.width, GameViewport.height);
-        context.moveTo(GameViewport.width, 0);
-        context.lineTo(0, GameViewport.height);
-        context.stroke();
+        // context.moveTo(0, 0);
+        // context.lineTo(GameViewport.width, GameViewport.height);
+        // context.moveTo(GameViewport.width, 0);
+        // context.lineTo(0, GameViewport.height);
+        // context.stroke();
         context.drawImage(ryu, position.x, position.y); 
         window.requestAnimationFrame(frame)
     }
 
     window.requestAnimationFrame(frame)
-    console.log(context);
+    // console.log(context);
 }
