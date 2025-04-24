339f25ab8fd6129af7d34bb519bacec5c554e8a7
willzin454 <williamcarvalho493@gmail.com>
2025-04-23 23:12:17 -0300

    Colocando as animações

--- src/entities/fighters/Iori.js
+++ src/entities/fighters/Iori.js
@@ -7,12 +7,16 @@ export class Iori extends Fighter {
         this.image = document.querySelector('img[alt="Iori"]');
 
         this.frames = new Map([
-            ['forwards-1', [2, 453, 76, 109]],
-            ['forwards-2', [78, 131, 60, 89]],
-            ['forwards-3', [152, 128, 64, 92]],
-            ['forwards-4', [229, 130, 63, 90]],
-            ['forwards-5', [307, 128, 54, 91]],
-            ['forwards-6', [371, 128, 50, 89]],
+            ['forwards-1', [2, 448, 76, 116]],
+            ['forwards-2', [81, 451, 58, 114]],
+            ['forwards-3', [144, 452, 58, 109]],
+            ['forwards-4', [204, 454, 54, 109]],
+            ['forwards-5', [262, 453, 60, 112]],
+            ['forwards-6', [326, 448, 61, 118]],
+            ['forwards-7', [388, 450, 52, 111]],
+            ['forwards-8', [441, 451, 62, 110]],
+            ['forwards-9', [509, 449, 75, 113]],
+            ['forwards-10', [583, 452, 82, 112]],
         ]);
     }
 }
