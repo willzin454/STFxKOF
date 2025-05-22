d49866d3d04ea0141af790d12d47978f99714dc1
willzin454 <williamcarvalho493@gmail.com>
2025-05-22 00:02:46 -0300

    Mov

--- src/StreetFighterGame.js
+++ src/StreetFighterGame.js
@@ -4,7 +4,7 @@ import { Stage } from "./entities/Stage.js";
 import { FpsCounter } from "./entities/FpsCounter.js";
 import { STAGE_FLOOR } from "./constants/stage.js";
 import { FighterDirection } from "./constants/fighter.js";  
-import { registerKeyboardEvents } from "./InputHandler.js";
+import { pollGamepads, registerGamepadEvents, registerKeyboardEvents } from "./InputHandler.js";
 import { Shadow } from "./entities/fighters/Shadow.js";
 
 export class StreetFighterGame {
@@ -55,12 +55,15 @@ export class StreetFighterGame {
             previous: time,
         }
 
+        pollGamepads();
         this.update();
         this.draw();
     }
 
     start(){  
         registerKeyboardEvents();
+        registerGamepadEvents();
+
         window.requestAnimationFrame(this.frame.bind(this));
     }
 }
--- src/constants/control.js
+++ src/constants/control.js
@@ -1,3 +1,9 @@
+export const GamepadThumbstick = {
+    DEAD_ZONE: 'deadZone',
+    HORIZONTAL_AXE_ID: 'horizontalAxeId',
+    VERTICAL_AXE_ID: 'verticalAxeId',
+};
+
 export const Control = {
     LEFT: 'left',
     RIGHT: 'right', 
@@ -7,14 +13,34 @@ export const Control = {
 
 export const controls = [
     {
+        gamePad: {
+            [GamepadThumbstick.DEAD_ZONE]: 0.5,
+            [GamepadThumbstick.HORIZONTAL_AXE_ID]: 0, // ✔️ número
+            [GamepadThumbstick.VERTICAL_AXE_ID]: 1,   // ✔️ número
+
+            [Control.LEFT]: 14,   // ✔️ número
+            [Control.RIGHT]: 15,  // ✔️ número
+            [Control.UP]: 12,     // ✔️ número
+            [Control.DOWN]: 13,   // ✔️ número
+        },
         keyboard: {
             [Control.LEFT]: 'KeyA',
             [Control.RIGHT]: 'KeyD',
             [Control.UP]: 'KeyW',
             [Control.DOWN]: 'KeyS',
-        }
+        },
     },
     {
+        gamePad: {
+            [GamepadThumbstick.DEAD_ZONE]: 0.5,
+            [GamepadThumbstick.HORIZONTAL_AXE_ID]: 0,
+            [GamepadThumbstick.VERTICAL_AXE_ID]: 1,
+
+            [Control.LEFT]: 14,
+            [Control.RIGHT]: 15,
+            [Control.UP]: 12,
+            [Control.DOWN]: 13,
+        },
         keyboard: {
             [Control.LEFT]: 'ArrowLeft',
             [Control.RIGHT]: 'ArrowRight',
